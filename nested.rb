734721edd6f68e266a9243dfc0464351ed0627cb
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-05 21:48:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,7 +119,7 @@ def changing_alan
     }
     
 
-alans_new_info = programmer_hash[:alan_kay]{:known_for => "GUI", :languages => ["C"]]
+alans_new_info = programmer_hash[:alan_kay] = {:known_for => "GUI", :languages => ["C"]}
 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
